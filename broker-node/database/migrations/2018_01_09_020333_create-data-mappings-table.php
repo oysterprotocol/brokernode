005b258cc6358f19ea12b6b67b4b9c9cb9ae0143
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-08 21:48:46 -0500

    Moves DataMapping model and creates migration script

---
+++ broker-node/database/migrations/2018_01_09_020333_create-data-mappings-table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataMappingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_mappings', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('genesis_hash', 255);
+            $table->string('hash', 255);
+            $table->integer('chunk_idx')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_mappings');
+    }
+}
