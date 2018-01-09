3a1371cb81171cb9ff3ea51564deb167a78af39b
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-09 10:39:52 -0500

    Integrates new version of DataMapping functions

--- broker-node/app/DataMapping.php
+++ broker-node/app/DataMapping.php
@@ -6,11 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataMapping extends Model
 {
-    protected $table = 'data_mappings';
+	protected $table = 'data_mappings';
+	protected $fillable = ['genesis_hash', 'hash', 'chunk_idx'];
 }
-
-include("Database.php");
-
 
 /**
  * Builds and loads the datamap in DB given genesis hash and the number of file chunks
@@ -19,14 +17,14 @@ include("Database.php");
  * @param integer $file_chunk_count the number to generate
  * @return boolean $result response code
  */
-function buildMap($genesis_hash, $file_chunk_count){
-
-
-    $Db = initializeDbConnection();
-
+function buildMap($genesis_hash, $file_chunk_count) {
     //we put the first chunk
-    $chunk_id = 0;
-    $Db->insertDataMapSection([[$genesis_hash, $chunk_id]]);
+    $chunk_idx = 0;
+    App\DataMapping::create([
+        'genesis_hash' => $genesis_hash,
+        'hash' => $genesis_hash,
+        'chunk_idx' => $chunk_idx
+    ]);
 
     //it takes the ceiling then disregards any past the correct number
     $hashes_per_db_update = 5;
@@ -35,30 +33,20 @@ function buildMap($genesis_hash, $file_chunk_count){
     //start process of getting hashes
     $hash_generator = hashGenerator($genesis_hash,  $file_chunk_count);
 
-    for($i = 0; $i < $num_groups; $i++){
+    for($i = 0; $i < $num_groups; $i++) {
+        $next_group = getNextNHashes($hash_generator, $hashes_per_db_update, $chunk_idx);
+        $chunk_idx += $hashes_per_db_update;
 
-        $next_group = getNextNHashes($hash_generator, $hashes_per_db_update, $chunk_id);
+        foreach($next_group as $chunk_idx_hash) {
+            [$curr_chunk_idx, $curr_hash] = $chunk_idx_hash;
 
-        $chunk_id += $hashes_per_db_update;
-
-        $Db->insertDataMapSection($next_group,$genesis_hash);
-
+            App\DataMapping::create([
+                'genesis_hash' => $genesis_hash,
+                'hash' => $curr_hash,
+                'chunk_idx' => $curr_chunk_idx
+            ]);
+        }
     }
-
-
-}
-
-/**
- * Separate out the Db initialization, this stuff will come from a config file
- */
-function initializeDbConnection(){
-    $server_name = 'localhost';
-    $user_name = 'admin';
-    $pass = 'nodeAdmin';
-    $db_name = 'BrokerNodeDatabase';
-
-    $DB = new BrokerDatabase($server_name, $user_name, $pass, $db_name);
-    return $DB;
 }
 
 /**
@@ -66,28 +54,22 @@ function initializeDbConnection(){
  *
  * @param generator $hash_generator yields the next hash value in the sequence
  * @param integer $n the number to generate
- * @param $chunk_id the chunk id to start at
+ * @param $chunk_idx the chunk id to start at
  * @return array Returns array of [chunk id , hash] pairs
  */
-function getNextNHashes($hash_generator, $n, $chunk_id){
+function getNextNHashes($hash_generator, $n, $chunk_idx) {
+	$next_group = array();
 
-    $next_group = array();
-
-    for($i = 0; $i < $n; $i++){
-
+    for($i = 0; $i < $n; $i++) {
         $hash_generator->next();
-
         $next_hash = $hash_generator->current();
-
         //this is returned if the generator is called too many times
-        if($next_hash == -1){
+        if($next_hash == -1) {
             break;
         }
 
-        $next_hash = [$hash_generator->current(), $chunk_id++];
-
+        $next_hash = [$hash_generator->current(), $chunk_idx++];
         array_push($next_group, $next_hash);
-
     }
 
     return $next_group;
@@ -101,22 +83,15 @@ function getNextNHashes($hash_generator, $n, $chunk_id){
  * @param integer $n the number to generate
  * @return User Returns User object or null if not found
  */
-function hashGenerator($genesis_hash, $n){
-
+function hashGenerator($genesis_hash, $n) {
     $hash = $genesis_hash;
 
-    for($i = 0; $i <= $n; $i++){
-
+    for($i = 0; $i <= $n; $i++) {
         yield $hash;
-
         $hash = hash("sha256", $hash);
 
     }
-    while(True){
+    while(True) {
         yield -1;
     }
 }
-
-//informal test
-$genesishash = "sdfsdfsdfs";
-buildMap($genesishash,  5);
