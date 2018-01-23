a3e31fcc35b1ff26635ffca22f7f10d6c3cdda28
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-23 18:07:47 -0500

    First pass at cron job flow

--- broker-node/app/Console/Commands/CheckChunkStatus.php
+++ broker-node/app/Console/Commands/CheckChunkStatus.php
@@ -3,9 +3,13 @@
 namespace App\Console\Commands;
 
 use App\DataMap;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
-class CheckChunkStatus extends Command {
+class CheckChunkStatus extends Command
+{
+    const HOOKNODE_TIMEOUT_THRESHOLD_MINUTES = 2000;
+
     protected $signature = 'CheckChunkStatus:checkStatus';
     protected $description =
         'Polls the status of chunks that have been sent to hook nodes';
@@ -21,6 +25,44 @@ class CheckChunkStatus extends Command {
      * Execute the console command.
      */
     public function handle() {
-        echo DataMap::all();
+        $thresholdTime =
+            Carbon::now()->subMinutes(self::HOOKNODE_TIMEOUT_THRESHOLD_MINUTES)
+                ->toDateTimeString();
+
+        /**
+         * Check pending datamaps.
+         */
+
+        $datamaps_pending =
+            DataMap::where('status', 'pending')
+                ->where('updated_at', '>=', $thresholdTime)
+                ->get();
+
+        $attached_datamaps = array_filter($datamaps_pending->toArray(), function($dmap) {
+            // TODO: Check status on tangle.
+            // TODO: Make these concurrent.
+            $is_attached = true; // placeholder.
+            return $is_attached;
+        });
+        $attached_ids = array_map(function($dmap) {
+            return $dmap["id"];
+        }, $attached_datamaps);
+
+        // Mass Update DB.
+        DataMap::whereIn('id', $attached_ids)->update('status', 'complete');
+
+        // TODO: Increment hooknode reputations for $attached_datamaps.
+
+        unset($datamaps_pending); // Purges unused memory.
+
+        /**
+         * Retry timedout datamaps.
+         */
+        $datamaps_timedout =
+            DataMap::where('status', 'pending')
+                ->where('updated_at', '<', $thresholdTime)
+                ->get();
+
+        // TODO: Retry with another hooknode.
     }
 }
