43113264ae81e3def591d6314f1b038958f2f315
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-27 17:13:04 -0500

    Installs better exception handler middleware for REST APIs

--- broker-node/bootstrap/app.php
+++ broker-node/bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Optimus\Heimdal\ExceptionHandler::class
 );
 
 /*
