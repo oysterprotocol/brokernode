3eac1bb698cc7a8696a06ef14487ad17a21031cd
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-01-28 12:08:51 -0500

    Implement model methods

--- broker-node/app/HookNode.php
+++ broker-node/app/HookNode.php
@@ -21,4 +21,24 @@ class HookNode extends Model
     protected $table = 'hook_nodes';
     protected $fillable = ['ip_address'];
     public $incrementing = false; // UUID
+
+    public static function insertNode($ip_address) {
+        return self::create(['ip_address' => $ip_address]);
+    }
+
+    public static function getHighestScoreNode() {
+        self::where('status', "TODO")
+            ->orderBy('score', 'desc')
+            ->firstOrFail();
+    }
+
+    public static function incrementScore($ip_address) {
+        self::where('ip_address', $ip_address)
+            ->increment('score', 1);
+    }
+
+    public static function decrementScore($ip_address) {
+        self::where('ip_address', $ip_address)
+            ->decrement('score', 1);
+    }
 }
