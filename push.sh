cec202d4b5b28a3ac84cc71622199160aced18c3
Alex <smigaja@gmail.com>
2018-02-07 14:09:19 -0800

    test git vm integration

--- push.sh
+++ push.sh
@@ -4,9 +4,9 @@ git add $(git rev-parse --show-toplevel)
 git commit -a 
 git checkout -b host
 git push -u origin host
-ssh oyster@192.168.56.20 'cd brokernode && git stash && git merge master host && git stash pop && git branch -D host' #change host if you want another branch name
+ssh oyster@192.168.56.20 'cd brokernode && git stash && git checkout host && git add $(git rev-parse --show-toplevel) && git commit -m "Test1" && git merge master host && git checkout master && git stash pop && git branch -D host' #change host if you want another branch name
 git checkout master
 git pull origin master
 git branch -D host
 echo "Done! This window will close in 10 seconds, just in case you need to look at any errors"
-sleep 10
+sleep 30
