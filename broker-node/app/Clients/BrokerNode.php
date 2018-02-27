469a599e6f1eb122d94863f92664423b17c6c049
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-02-27 13:03:46 -0500

    Installs segment client with sample log

--- broker-node/app/Clients/BrokerNode.php
+++ broker-node/app/Clients/BrokerNode.php
@@ -212,8 +212,17 @@ class BrokerNode
         NodeMessenger::sendMessageToNodesAndContinue($tx, $hookNodes);
 
         //record event
-        self::initEventRecord();
         self::$ChunkEventsRecord->addChunkEvent("chunk_sent_to_hook", $hookNodeUrl, "todo", "todo");
+        Segment::track([
+            "event" => "chunk_sent_to_hook",
+            "properties" => [
+                "broker_url" => $_SERVER['REMOTE_ADDR'],
+                "hooknode_url" => $hookNodeUrl,
+            ]
+        ]);
+
+        // DEPRECATED. This will be replaced with segment.io
+        self::initEventRecord();
         HookNode::incrementChunksProcessed($hookNodeUrl, count($chunks));
 
         array_walk($chunks, function ($chunk) use ($hookNodeUrl, $request) {
---
+++ broker-node/config/segment.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Alt Three Segment.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Segment Write Key
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies key which enables you to write to Segment's API.
+    |
+    | Default: ''
+    |
+    */
+
+    'write_key' => env('SEGMENT_WRITE_KEY'),
+
+];
