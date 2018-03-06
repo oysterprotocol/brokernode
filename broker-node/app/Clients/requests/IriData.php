00d8587b1b8a266a117e8e1b4d79bbbb50ff328c
Rebel Fornea <rebelfornea@gmail.com>
2018-03-06 14:43:02 -0500

    Setting back to files from master

--- broker-node/app/Clients/requests/IriData.php
+++ broker-node/app/Clients/requests/IriData.php
@@ -4,36 +4,6 @@ namespace App\Clients\requests;
 
 class IriData
 {
-    public static $oysterTag = 'AUDIOTESTTWO';  //will use this as the 'tag'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static $nodeUrl = "http://172.17.0.1:14265";
     public static $apiVersion = '1.4';
 
@@ -42,29 +12,8 @@ class IriData
     public static $minWeightMagnitude = 14;
     public static $minMinWeightMagnitude = 14;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static $oysterSeed = 'OYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRL';
+    public static $oysterTag = 'OYSTERPRL';  //will use this as the 'tag'
     public static $txValue = 0;
 
     public static $maxIRICallAttempts = 50;  //we need to discuss this
