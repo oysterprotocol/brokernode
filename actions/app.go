1ce0a0324ca2a02775e5fc5e94337d8998360ef3
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-03-13 17:34:05 -0400

    Bootstrap a go api server.

---
+++ actions/app.go
@@ -0,0 +1,56 @@
+package actions
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/gobuffalo/buffalo/middleware/ssl"
+	"github.com/gobuffalo/envy"
+	"github.com/unrolled/secure"
+
+	"github.com/gobuffalo/x/sessions"
+	"github.com/oysterprotocol/brokernode/models"
+	"github.com/rs/cors"
+)
+
+// ENV is used to help switch settings based on where the
+// application is being run. Default is "development".
+var ENV = envy.Get("GO_ENV", "development")
+var app *buffalo.App
+
+// App is where all routes and middleware for buffalo
+// should be defined. This is the nerve center of your
+// application.
+func App() *buffalo.App {
+	if app == nil {
+		app = buffalo.New(buffalo.Options{
+			Env:          ENV,
+			SessionStore: sessions.Null{},
+			PreWares: []buffalo.PreWare{
+				cors.Default().Handler,
+			},
+			SessionName: "_brokernode_session",
+		})
+		// Automatically redirect to SSL
+		app.Use(ssl.ForceSSL(secure.Options{
+			SSLRedirect:     ENV == "production",
+			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
+		}))
+
+		// Set the request content type to JSON
+		app.Use(middleware.SetContentType("application/json"))
+
+		if ENV == "development" {
+			app.Use(middleware.ParameterLogger)
+		}
+
+		// Wraps each request in a transaction.
+		//  c.Value("tx").(*pop.PopTransaction)
+		// Remove to disable this.
+		app.Use(middleware.PopTransaction(models.DB))
+
+		app.GET("/", HomeHandler)
+
+	}
+
+	return app
+}
