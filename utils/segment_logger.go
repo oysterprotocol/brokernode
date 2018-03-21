d3b2149217260885cc1ad26ee9277d170bfb4b7c
Rebel Fornea <rebelfornea@gmail.com>
2018-03-21 11:42:56 -0400

    Forgot to comment all this out again.

--- utils/segment_logger.go
+++ utils/segment_logger.go
@@ -1,53 +1,54 @@
 package oyster_utils
 
 import (
-	"os"
+	//"os"
 	//"github.com/joho/godotenv"
-	"net"
-	"time"
+	//"net"
+	//"time"
 	//"github.com/segmentio/analytics-go"
 	//"gopkg.in/segmentio/analytics-go.v3"
-	"gopkg.in/segmentio/analytics-go.v3"
-	"github.com/joho/godotenv"
+	//"gopkg.in/segmentio/analytics-go.v3"
+	//"github.com/joho/godotenv"
 	//"log"
+	//"gopkg.in/segmentio/analytics-go.v3"
 )
 
-var SegmentClient analytics.Client
+//var SegmentClient analytics.Client
 
 func init() {
 	// Load ENV variables
-	err := godotenv.Load()
-	if err != nil {
-		//log.Fatal("Error loading .env file")
-	}
-
-	// Setup Segment
-	SegmentClient = analytics.New(os.Getenv("SEGMENT_WRITE_KEY"))
+	//err := godotenv.Load()
+	//if err != nil {
+	//	//log.Fatal("Error loading .env file")
+	//}
+	//
+	//// Setup Segment
+	//SegmentClient = analytics.New(os.Getenv("SEGMENT_WRITE_KEY"))
 }
 
 func GetLocalIP() string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return ""
-	}
-	for _, address := range addrs {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String()
-			}
-		}
-	}
+	//addrs, err := net.InterfaceAddrs()
+	//if err != nil {
+	//	return ""
+	//}
+	//for _, address := range addrs {
+	//	// check the address type and if it is not a loopback the display it
+	//	if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+	//		if ipnet.IP.To4() != nil {
+	//			return ipnet.IP.String()
+	//		}
+	//	}
+	//}
 	return ""
 }
 
-func TimeTrack(start time.Time, name string, properties analytics.Properties) {
-	elapsed := time.Since(start).Seconds()
-
-	go SegmentClient.Enqueue(analytics.Track{
-		Event:  name,
-		UserId: GetLocalIP(),
-		Properties: properties.
-			Set("time_elapsed", elapsed),
-	})
-}
+//func TimeTrack(start time.Time, name string, properties analytics.Properties) {
+	//elapsed := time.Since(start).Seconds()
+	//
+	//go SegmentClient.Enqueue(analytics.Track{
+	//	Event:  name,
+	//	UserId: GetLocalIP(),
+	//	Properties: properties.
+	//		Set("time_elapsed", elapsed),
+	//})
+//}
