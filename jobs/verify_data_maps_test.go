061d14011654afeae4dc42a9a785cfae320ee9b7
Rebel Fornea <rebelfornea@gmail.com>
2018-03-22 20:59:14 -0400

    -adds methods to iota wrapper that will verify if a chunk is on the tangle, and if so, if the message, trunk, and branch are correct.  I tested this manually with real tangle transactions and it works -adds a "generic" iota mocker to the job runner test file.  We can use this in other test files, and modify the mocking methods in other test files as needed -attaches iota methods to a struct, will need to pass this in as a dependency to cron methods

--- jobs/verify_data_maps_test.go
+++ jobs/verify_data_maps_test.go
@@ -2,26 +2,93 @@ package jobs_test
 
 import (
 	"github.com/oysterprotocol/brokernode/models"
+	"github.com/oysterprotocol/brokernode/jobs"
+	"github.com/oysterprotocol/brokernode/services"
 )
 
+var Suite JobsSuite
+var processChunksMockCalled = false
+var verifyChunkMessagesMatchesRecordMockCalled = false
+
 func (suite *JobsSuite) Test_VerifyDataMaps() {
+
+	// make suite available inside mock methods
+	Suite = *suite
+
+	// assign the mock methods for this test
+	makeMocks(&IotaMock)
+
+	// populate data_maps
 	genHash := "someGenHash"
-	fileBytesCount := 30000
+	fileBytesCount := 18000
 
 	vErr, err := models.BuildDataMaps(genHash, fileBytesCount)
 	suite.Nil(err)
 	suite.Equal(0, len(vErr.Errors))
 
+	// check that it is the length we expect
 	allDataMaps := []models.DataMap{}
-
 	err = suite.DB.All(&allDataMaps)
-	suite.Equal(12, len(allDataMaps))
+	suite.Equal(8, len(allDataMaps))
 
-	// make half the data maps "Unverified"
-	for i := 0; i < (len(allDataMaps) / 2); i++ {
+	// make first 6 data maps unverified
+	for i := 0; i < 6; i++ {
 		allDataMaps[i].Status = models.Unverified
 		suite.DB.ValidateAndSave(&allDataMaps[i])
 	}
 
-	// will have to wait until we can mock these iota methods
+	// call method under test, passing in our mock of our iota methods
+	jobs.VerifyDataMaps(IotaMock)
+
+	// verify the mock methods were called
+	suite.Equal(true, processChunksMockCalled)
+	suite.Equal(true, verifyChunkMessagesMatchesRecordMockCalled)
+
+	// reset back to generic mocks
+	suite.SetupSuite()
+}
+
+func makeMocks(iotaMock *services.IotaService) {
+
+	iotaMock.VerifyChunkMessagesMatchRecord = verifyChunkMessagesMatchesRecordMock
+	iotaMock.ProcessChunks = processChunksMock
+}
+
+func verifyChunkMessagesMatchesRecordMock(chunks []models.DataMap) (filteredChunks services.FilteredChunk, err error) {
+
+	// our mock was called
+	verifyChunkMessagesMatchesRecordMockCalled = true
+
+	allDataMaps := []models.DataMap{}
+	err = Suite.DB.All(&allDataMaps)
+	Suite.Nil(err)
+
+	matchesTangle := []models.DataMap{}
+	doesNotMatchTangle := []models.DataMap{}
+	notAttached := []models.DataMap{}
+
+	// assign some data_maps to different arrays to be returned when we filter the chunks
+	matchesTangle = append(matchesTangle, allDataMaps[0], allDataMaps[3], allDataMaps[4], allDataMaps[5])
+	notAttached = append(notAttached, allDataMaps[1])
+	// the contents of 'doesNotMatchTangle' is what we will be checking for later
+	doesNotMatchTangle = append(doesNotMatchTangle, allDataMaps[2])
+
+	return services.FilteredChunk{
+		MatchesTangle:      matchesTangle,
+		NotAttached:        notAttached,
+		DoesNotMatchTangle: doesNotMatchTangle,
+	}, err
+}
+
+func processChunksMock(chunks []models.DataMap, attachIfAlreadyAttached bool) {
+
+	// our mock was called
+	processChunksMockCalled = true
+
+	allDataMaps := []models.DataMap{}
+	var err = Suite.DB.All(&allDataMaps)
+	Suite.Nil(err)
+
+	// ProcessChunks should have gotten called with the index 2 data map from data_maps
+	Suite.Equal(chunks[0], allDataMaps[2])
 }
--- services/iota_wrappers.go
+++ services/iota_wrappers.go
@@ -1,10 +1,31 @@
 package services
 
 import (
+	"fmt"
+	"github.com/iotaledger/giota"
 	"github.com/oysterprotocol/brokernode/models"
+	"os"
+	"strings"
+	"sync"
 )
 
-type IotaWrapper struct {
+type PowJob struct {
+	Transactions      []giota.Transaction
+	TrunkTransaction  giota.Trytes
+	BranchTransaction giota.Trytes
+	BroadcastNodes    []string
+}
+
+type ProcessChunks func(chunks []models.DataMap, attachIfAlreadyAttached bool)
+type VerifyChunkMessagesMatchRecord func(chunks []models.DataMap) (filteredChunks FilteredChunk, err error)
+type VerifyChunksMatchRecord func(chunks []models.DataMap, checkChunkAndBranch bool) (filteredChunks FilteredChunk, err error)
+type ChunksMatch func(chunkOnTangle giota.Transaction, chunkOnRecord models.DataMap, checkBranchAndTrunk bool) bool
+
+type IotaService struct {
+	ProcessChunks                  ProcessChunks
+	VerifyChunkMessagesMatchRecord VerifyChunkMessagesMatchRecord
+	VerifyChunksMatchRecord        VerifyChunksMatchRecord
+	ChunksMatch                    ChunksMatch
 }
 
 type FilteredChunk struct {
@@ -13,10 +34,123 @@ type FilteredChunk struct {
 	NotAttached        []models.DataMap
 }
 
-func VerifyChunkMessageMatchesTangle(chunks []models.DataMap) []models.DataMap {
-	return chunks
+var seed giota.Trytes
+
+var provider = os.Getenv("PROVIDER")
+var minDepth = int64(giota.DefaultNumberOfWalks)
+var minWeightMag = int64(1)
+
+var api = giota.NewAPI(provider, nil)
+var bestPow giota.PowFunc
+var powName string
+
+// Things below are copied from the giota lib since they are not public.
+// https://github.com/iotaledger/giota/blob/master/transfer.go#L322
+
+// (3^27-1)/2
+const maxTimestampTrytes = "MMMMMMMMM"
+
+// This mutex was added by us.
+var mutex = &sync.Mutex{}
+var IotaWrapper IotaService
+
+func init() {
+	seed = "OYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRLOYSTERPRL"
+
+	powName, bestPow = giota.GetBestPoW()
+
+	IotaWrapper = IotaService{
+		ProcessChunks:                  processChunks,
+		VerifyChunkMessagesMatchRecord: verifyChunkMessagesMatchRecord,
+		VerifyChunksMatchRecord:        verifyChunksMatchRecord,
+		ChunksMatch:                    chunksMatch,
+	}
 }
 
-func ProcessChunks(chunks []models.DataMap, reattachIfAlreadyAttached bool) []models.DataMap {
-	return chunks
+func processChunks(chunks []models.DataMap, attachIfAlreadyAttached bool) {
+	fmt.Println(chunks)
+}
+
+func verifyChunkMessagesMatchRecord(chunks []models.DataMap) (filteredChunks FilteredChunk, err error) {
+	filteredChunks, err = verifyChunksMatchRecord(chunks, false)
+	return filteredChunks, err
+}
+
+func verifyChunksMatchRecord(chunks []models.DataMap, checkChunkAndBranch bool) (filteredChunks FilteredChunk, err error) {
+
+	addresses := make([]giota.Address, 0, len(chunks))
+
+	for _, chunk := range chunks {
+		addresses = append(addresses, giota.Address(chunk.Address))
+	}
+
+	request := giota.FindTransactionsRequest{
+		Command:   "findTransactions",
+		Addresses: addresses,
+	}
+
+	response, err := api.FindTransactions(&request)
+
+	if err != nil {
+		fmt.Println(err)
+		return filteredChunks, err
+	}
+
+	filteredChunks = FilteredChunk{}
+
+	if response != nil && len(response.Hashes) > 0 {
+		trytesArray, err := api.GetTrytes(response.Hashes)
+		if err != nil {
+			fmt.Println(err)
+			return filteredChunks, err
+		}
+
+		transactionObjects := map[giota.Address][]giota.Transaction{}
+
+		for _, txObject := range trytesArray.Trytes {
+			transactionObjects[txObject.Address] = append(transactionObjects[txObject.Address], txObject)
+		}
+
+		for _, chunk := range chunks {
+
+			if _, ok := transactionObjects[giota.Address(chunk.Address)]; ok {
+				matchFound := false
+				for _, txObject := range transactionObjects[giota.Address(chunk.Address)] {
+					if chunksMatch(txObject, chunk, checkChunkAndBranch) {
+						matchFound = true
+						break
+					}
+				}
+				if matchFound {
+					filteredChunks.MatchesTangle = append(filteredChunks.MatchesTangle, chunk)
+				} else {
+					filteredChunks.DoesNotMatchTangle = append(filteredChunks.DoesNotMatchTangle, chunk)
+				}
+			} else {
+				filteredChunks.NotAttached = append(filteredChunks.NotAttached, chunk)
+			}
+		}
+	} else if len(response.Hashes) == 0 {
+		filteredChunks.NotAttached = chunks
+	}
+	return filteredChunks, err
+}
+
+func chunksMatch(chunkOnTangle giota.Transaction, chunkOnRecord models.DataMap, checkBranchAndTrunk bool) bool {
+
+	if checkBranchAndTrunk == false &&
+		strings.Contains(fmt.Sprint(chunkOnTangle.SignatureMessageFragment), chunkOnRecord.Message) == true {
+
+		return true
+
+	} else if strings.Contains(fmt.Sprint(chunkOnTangle.SignatureMessageFragment), chunkOnRecord.Message) == true &&
+		strings.Contains(fmt.Sprint(chunkOnTangle.TrunkTransaction), chunkOnRecord.TrunkTx) &&
+		strings.Contains(fmt.Sprint(chunkOnTangle.BranchTransaction), chunkOnRecord.BranchTx) {
+
+		return true
+
+	} else {
+
+		return false
+	}
 }
