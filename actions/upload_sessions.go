00746e3adc95de4f62715f30fb0ba06f8f19ea06
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-03-27 20:55:14 -0400

    Beta endpoint works

--- actions/upload_sessions.go
+++ actions/upload_sessions.go
@@ -50,5 +50,23 @@ func (usr *UploadSessionResource) Create(c buffalo.Context) error {
 
 // CreateBeta creates an upload session on the beta broker.
 func (usr *UploadSessionResource) CreateBeta(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"this-works": "nice"}))
+	req := uploadSessionCreateReq{}
+	parseReqBody(c.Request(), &req)
+
+	u := models.UploadSession{
+		Type:          models.SessionTypeBeta,
+		GenesisHash:   req.GenesisHash,
+		FileSizeBytes: req.FileSizeBytes,
+	}
+	vErr, err := u.StartUploadSession()
+	if err != nil {
+		return err
+	}
+
+	if len(vErr.Errors) > 0 {
+		c.Render(422, r.JSON(vErr.Errors))
+		return err
+	}
+
+	return c.Render(200, r.JSON(u))
 }
--- actions/upload_sessions_test.go
+++ actions/upload_sessions_test.go
@@ -25,3 +25,22 @@ func (as *ActionSuite) Test_UploadSessionsCreate() {
 	as.Equal(123, resParsed.FileSizeBytes)
 	as.Equal(models.SessionTypeAlpha, resParsed.Type)
 }
+
+func (as *ActionSuite) Test_UploadSessionsCreateBeta() {
+	res := as.JSON("/api/v2/upload-sessions/beta").Post(map[string]interface{}{
+		"genesisHash":   "genesisHashTest",
+		"fileSizeBytes": 123,
+	})
+
+	// Parse response
+	resParsed := uploadSessionCreateRes{}
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	as.Nil(err)
+	err = json.Unmarshal(bodyBytes, &resParsed)
+	as.Nil(err)
+
+	as.Equal(200, res.Code)
+	as.Equal("genesisHashTest", resParsed.GenesisHash)
+	as.Equal(123, resParsed.FileSizeBytes)
+	as.Equal(models.SessionTypeBeta, resParsed.Type)
+}
