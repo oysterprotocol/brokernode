ca32d175aaa8db8057d748d7cf6da9b94473065e
Edmund Mai <edmundmai@gmail.com>
2018-04-04 17:24:57 -0400

    fixing

--- actions/transaction_brokernodes.go
+++ actions/transaction_brokernodes.go
@@ -1,8 +1,10 @@
 package actions
 
 import (
+	"fmt"
 	"github.com/gobuffalo/buffalo"
 	"github.com/oysterprotocol/brokernode/models"
+	"github.com/pkg/errors"
 )
 
 type TransactionBrokernodeResource struct {
@@ -10,6 +12,10 @@ type TransactionBrokernodeResource struct {
 }
 
 // Request Response structs
+
+type transactionCreateReq struct {
+	CurrentList []string `json:"currentList"`
+}
 
 type transactionCreateRes struct {
 	ID models.Transaction `json:"id"`
@@ -20,10 +26,23 @@ func (usr *TransactionBrokernodeResource) Create(c buffalo.Context) error {
 	req := transactionCreateReq{}
 	parseReqBody(c.Request(), &req)
 
+	fmt.Println("xxxxxxxxxxxxxxxxxxxx")
+	tx := models.DB
+	query := tx.Limit(1).Where("status = 'unassigned' LIMIT 1")
+	dataMap := models.DataMap{}
+	err := query.All(&dataMap)
+
+	if err != nil {
+		c.Render(400, r.JSON(map[string]string{"Error finding session": errors.WithStack(err).Error()}))
+		return err
+	}
+
+	fmt.Println(dataMap)
+
 	t := models.Transaction{
 		Type:      "BROKERNODE",
 		Status:    "PAYMENT_PENDING",
-		DataMapID: "xxxxx",
+		DataMapID: dataMap.ID,
 		Purchase:  "xxxxx",
 	}
 
