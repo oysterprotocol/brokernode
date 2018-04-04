833f57ce980fa9a02f4960562d45877199bd4965
Edmund Mai <edmundmai@gmail.com>
2018-04-04 15:22:09 -0400

    transactions model + migrations

---
+++ models/transaction.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"encoding/json"
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+	"time"
+)
+
+type Transaction struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Status    string    `json:"status" db:"status"`
+	Type      string    `json:"type" db:"type"`
+	DataMapID uuid.UUID `json:"data_map_id" db:"data_map_id"`
+	Purchase  string    `json:"purchase" db:"purchase"`
+}
+
+// String is not required by pop and may be deleted
+func (t Transaction) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Transactions is not required by pop and may be deleted
+type Transactions []Transaction
+
+// String is not required by pop and may be deleted
+func (t Transactions) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (t *Transaction) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: t.Status, Name: "Status"},
+		&validators.StringIsPresent{Field: t.Type, Name: "Type"},
+		&validators.StringIsPresent{Field: t.Purchase, Name: "Purchase"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (t *Transaction) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (t *Transaction) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
