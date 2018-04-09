84d87f598ed9a6bb71dbcf63c7f6c91989449ad7
Edmund Mai <edmundmai@gmail.com>
2018-04-09 14:30:55 -0400

    why

--- actions/transaction_brokernodes.go
+++ actions/transaction_brokernodes.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 
 	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
 	"github.com/oysterprotocol/brokernode/models"
-	"github.com/pkg/errors"
 )
 
 type TransactionBrokernodeResource struct {
@@ -29,36 +29,32 @@ func (usr *TransactionBrokernodeResource) Create(c buffalo.Context) error {
 	req := transactionCreateReq{}
 	parseReqBody(c.Request(), &req)
 
-	fmt.Println("DATAMAPPPPPPPPPPPPPPPP")
-	tx := models.DB
 	dataMap := models.DataMap{}
-	dataMapError := tx.Limit(1).Where("status = 'unassigned'").First(&dataMap)
+	brokernode := models.Brokernode{}
+	t := models.Transaction{}
 
+	models.DB.Limit(1).Where("status = ?", models.Unassigned).First(&dataMap)
+	fmt.Println("datamappppppppppppppppppppppppppp")
 	fmt.Println(dataMap)
 
-	if dataMapError != nil {
-		c.Render(400, r.JSON(map[string]string{"Error finding session": errors.WithStack(dataMapError).Error()}))
-		return dataMapError
-	}
-
-	fmt.Println("BROKERNODEEEEEEEEEEEE")
-	brokernode := models.Brokernode{}
 	existingAddresses := join(req.CurrentList, ", ")
-	brokernodeError := tx.Limit(1).Where("address NOT IN (?)", existingAddresses).First(&brokernode)
-
+	models.DB.Limit(1).Where("address NOT IN (?)", existingAddresses).First(&brokernode)
+	fmt.Println("brokernodeeeeeeeeeee")
 	fmt.Println(brokernode)
 
-	if brokernodeError != nil {
-		c.Render(400, r.JSON(map[string]string{"Error finding session": errors.WithStack(brokernodeError).Error()}))
-		return brokernodeError
-	}
+	models.DB.Transaction(func(tx *pop.Connection) error {
+		dataMap.Status = models.Unverified
+		tx.ValidateAndSave(&dataMap)
 
-	t := models.Transaction{
-		Type:      "BROKERNODE",
-		Status:    "PAYMENT_PENDING",
-		DataMapID: dataMap.ID,
-		Purchase:  "xxxxx",
-	}
+		fmt.Println("transssssssssssss")
+		t = models.Transaction{
+			Type:      "BROKERNODE",
+			Status:    "PAYMENT_PENDING",
+			DataMapID: dataMap.ID,
+			Purchase:  brokernode.Address,
+		}
+		return nil
+	})
 
 	res := transactionCreateRes{
 		ID: t.ID,
