37e8afefe08ec8ce1efd86451eeba7a2ff33c99e
Rebel Fornea <rebelfornea@gmail.com>
2018-04-18 14:38:07 -0400

    updating main.go and gitignore

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/gobuffalo/pop"
 	"github.com/oysterprotocol/brokernode/actions"
 	"log"
 )
 
 func main() {
+	pop.Debug = false
 	app := actions.App()
 	if err := app.Serve(); err != nil {
 		log.Fatal(err)
