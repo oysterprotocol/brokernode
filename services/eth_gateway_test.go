263cc1b0327cfadae39bada8df410e169e83a03e
Astor Rivera <arivera@bunkercapital.com>
2018-05-23 13:50:23 -0700

    WIP fixed deploy contract on simulator which was having gas price issues.

--- services/eth_gateway_test.go
+++ services/eth_gateway_test.go
@@ -5,17 +5,19 @@ import (
 
 	"github.com/oysterprotocol/brokernode/services"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"os"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"strings"
 	"math/big"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
 	"github.com/ethereum/go-ethereum/core"
-	"log"
-	"context"
 	"time"
+	"github.com/ethereum/go-ethereum/params"
+	"log"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"os"
+	"strings"
+	"context"
+	"github.com/oysterprotocol/brokernode/models"
 )
 
 //
@@ -113,53 +115,66 @@ func Test_getCurrentBlockGasLimit(t *testing.T) {
 	}
 }
 
+
+// wip - send gas(ether) to an address for a transaction
+func Test_sendEth(t *testing.T) {
+	t.Skip(nil)
+	// test account accepting ether
+	testAcct := common.HexToAddress("f10a2706e98ef86b6866ae6cab2e0ca501fdf091")
+	transferValue := new(big.Int).SetUint64(1)
+
+	// Send ether to test account
+	raw, err := services.EthWrapper.SendETH(testAcct, transferValue)
+	if err != nil {
+		t.Logf("failed to send ether to %v ether to %v", transferValue, testAcct.Hex())
+		t.Fatalf("transaction error: %v", err)
+	}
+	t.Logf("raw transaction : %v", raw)
+}
+
+//
+// Oyster Pearl Contract Tests
+//
+
 // simulated blockchain to deploy oyster pearl
 func Test_deployOysterPearl(t *testing.T) {
 
-	// Generate a new random account and a funded simulator
+	// generate a new random account and a funded simulator
 	key, _ := crypto.GenerateKey()
 	auth := bind.NewKeyedTransactor(key)
-
-	// genesis
-	genesis := make(core.GenesisAlloc)
-	genesis[auth.From] = core.GenesisAccount{
-		Balance: big.NewInt(100000),
-		Nonce:uint64(0),
-	}
-
-	// simulator
-	sim := backends.NewSimulatedBackend(genesis)
+	sim := backends.NewSimulatedBackend(core.GenesisAlloc{auth.From: {Balance: big.NewInt(990000000000)}})
 
 	// initialize the context
 	deadline := time.Now().Add(1000 * time.Millisecond)
 	ctx, cancel := context.WithDeadline(context.Background(), deadline)
 	defer cancel()
 
-	// Deploy a token contract on the simulated blockchain
-	newAddress, transaction, token, err := services.DeployOysterPearl(&bind.TransactOpts{
+	// deploy a token contract on the simulated blockchain
+	_, _, token, err := services.DeployOysterPearl(&bind.TransactOpts{
 		Nonce:big.NewInt(0),
 		From:auth.From,
-		GasLimit:uint64(30000),
-		GasPrice: big.NewInt(25000),
+		GasLimit:params.GenesisGasLimit,
+		GasPrice: big.NewInt(1),
 		Context:  ctx,
 		Signer:auth.Signer,
 	}, sim)
 	if err != nil {
 		log.Fatalf("Failed to deploy oyster token contract: %v", err)
 	}
-	t.Logf("newAddress :%v", newAddress)
-	t.Logf("transaction :%v", transaction)
 	t.Logf("token :%v", token)
 
-	// Commit all pending transactions in the simulator and print the names again
+	// Commit all pending transactions in the simulator
 	sim.Commit()
+
+
 }
 
-// test sending PRLs from OysterPearl Contract
+// testing token name access from OysterPearl Contract
+// basic test which validates the existence of the contract on the network
 func Test_tokenNameFromOysterPearl(t *testing.T) {
-	t.Skip(nil)
 	// test ethClient
 	var backend, _ = ethclient.Dial("http://54.86.134.172:8080")
+	// prl test 1 0x1be77862769ab791c4f95f8a2cbd0d3e07a3fd1f
 	oysterPearl, err := services.NewOysterPearl(common.HexToAddress("0x84e07b9833af3d3c8e07b71b1c9c041ec5909d5d"), backend)
 	if err != nil {
 		t.Fatalf("unable to access contract instance at :%v",err)
@@ -173,15 +188,6 @@ func Test_tokenNameFromOysterPearl(t *testing.T) {
 
 // test sending PRLs from OysterPearl Contract
 func Test_transferPRLFromOysterPearl(t *testing.T) {
-
-	//file, err := ioutil.ReadFile("./testdata/key.json")
-	//if err != nil {
-	//	t.Fatalf("error reading key file")
-	//}
-	//var key map[string]interface{}
-	//json.Unmarshal(file, &key)
-	////address := key["address"]
-	//t.Logf("key :%v", key)
 
 	// test ethClient
 	var backend, _ = ethclient.Dial("http://54.86.134.172:8080")
@@ -199,8 +205,8 @@ func Test_transferPRLFromOysterPearl(t *testing.T) {
 		t.Fatalf("unable to create a new transactor :%v", err)
 	}
 
-	//testPRLAcct := common.HexToAddress("f10a2706e98ef86b6866ae6cab2e0ca501fdf091")
-	//testPRLAcct := common.HexToAddress("0xDf7D0030bfed998Db43288C190b63470c2d18F50")
+	//test1PRLAcct := common.HexToAddress("0x1be77862769ab791c4f95f8a2cbd0d3e07a3fd1f")
+	//test2PRLAcct := common.HexToAddress("0x74ad69b41e71e311304564611434ddd59ee5d1f8")
 
 	// wrap the oyster pearl contract instance into a session
 	session := &services.OysterPearlSession{
@@ -216,55 +222,16 @@ func Test_transferPRLFromOysterPearl(t *testing.T) {
 	}
 	session.Name()
 	session.Transfer(common.HexToAddress("0x0000000000000000000000000000000000000000"), big.NewInt(1))
-
 }
 
-/*
-func Test_sendEth(t *testing.T) {
-
-	// test account accepting ether
-	testAcct := common.HexToAddress("f10a2706e98ef86b6866ae6cab2e0ca501fdf091")
-	transferValue := new(big.Int).SetUint64(1)
-
-	// Send ether to test account
-	raw, err := services.EthWrapper.SendETH(testAcct, transferValue)
-	if err != nil {
-		t.Logf("failed to send ether to %v ether to %v", transferValue, testAcct.Hex())
-		t.Fatalf("transaction error: %v", err)
-	}
-	t.Logf("raw transaction : %v", raw)
-}
-*/
-
-/*
-// send gas for a transaction
-func (s *EthereumTestSuite) sendGas(t *testing.T) {
-	// Send Gas to an Account
-	// WIP - Add once we update the send via contract method
-}
-
-// send ether
-func (s *EthereumTestSuite) sendEther(t *testing.T) {
-	// Send Ether to an Account
-	// WIP - Add once we update the send via contract method
-}
-*/
-//
-// Oyster Pearl Contract Tests
-//
-/*
-// deploy the compiled oyster contract to Oysterby network
-func (s *EthereumTestSuite) deployContractOnOysterby(t *testing.T) {
-}
-*/
 //
 // Oyster Pearl Tests
-//
+// all tests below will exercise the contract methods in the simulator and the private oysterby network.
 
-// bury prl
-/*
+
+// bury prl TODO provide simulated backend test and private network test
 func Test_buryPRL(t *testing.T) {
-
+	t.Skip(nil)
 	// PRL based addresses
 	from := common.HexToAddress("0x68e97d19da3b8a0dff21e2ac6bf1b7f63d4e7360")
 	to := common.HexToAddress("0xf10a2706e98ef86b6866ae6cab2e0ca501fdf091")
@@ -294,11 +261,10 @@ func Test_buryPRL(t *testing.T) {
 		t.Fatal("Faild to bury PRLs. Try Again?")
 	}
 }
-*/
-// send prl
-/*
-func (s *EthereumTestSuite) sendPRL(t *testing.T) {
 
+// send prl TODO provide simulated backend test and private network test
+func Test_sendPRL(t *testing.T) {
+	t.Skip(nil)
 	// prepare oyster message call
 	var msg = services.OysterCallMsg{
 		From:     common.HexToAddress("0x0d1d4e623d10f9fba5db95830f7d3839406c6af2"),
@@ -311,7 +277,7 @@ func (s *EthereumTestSuite) sendPRL(t *testing.T) {
 	}
 
 	// Send PRL
-	var sent = s.gateway.SendPRL(msg)
+	var sent = services.EthWrapper.SendPRL(msg)
 	if sent {
 		// successful prl send
 		t.Logf("Sent PRL to :%v", msg.From.Hex())
@@ -320,12 +286,10 @@ func (s *EthereumTestSuite) sendPRL(t *testing.T) {
 		t.Fatalf("Failed to send PRL to:%v", msg.From.Hex())
 	}
 }
-*/
 
-/*
-// claim prl
-func (s *EthereumTestSuite) claimPRL(t *testing.T) {
-
+// claim prl TODO provide simulated backend test and private network test
+func Test_claimPRL(t *testing.T) {
+	t.Skip(nil)
 	// Receiver
 	receiverAddress := common.HexToAddress("0xC30efFC3509D56ef748d51f9580c81ff8e9c610E")
 
@@ -334,19 +298,19 @@ func (s *EthereumTestSuite) claimPRL(t *testing.T) {
 	treasurePrivateKey := "8d5366123cb560bb606379f90a0bfd4769eecc0557f1b362dcae9012b548b1e5"
 
 	// Claim PRL
-	claimed := s.gateway.ClaimPRL(receiverAddress, treasureAddress, treasurePrivateKey)
+	claimed := services.EthWrapper.ClaimPRL(receiverAddress, treasureAddress, treasurePrivateKey)
 	if !claimed {
 		t.Fatal("Failed to claim PRLs")
 	} else {
 		t.Log("PRLs have been successfully claimed")
 	}
 
-}*/
+}
 
-/*
-// claim unused prl from completed upload
-func (s *EthereumTestSuite) claimUnusedPRL(t *testing.T) {
 
+// claim unused prl from completed upload TODO provide simulated backend test and private network test
+func Test_claimUnusedPRL(t *testing.T) {
+	t.Skip(nil)
 	// Need to fake the completed uploads by populating with data
 	var rowWithGasTransferSuccess = models.CompletedUpload{
 		GenesisHash:   "RowWithGasTransferSuccess",
@@ -360,7 +324,7 @@ func (s *EthereumTestSuite) claimUnusedPRL(t *testing.T) {
 	completedUploads := []models.CompletedUpload{rowWithGasTransferSuccess}
 
 	// Claim PRL
-	err := s.gateway.ClaimUnusedPRLs(completedUploads)
+	err := services.EthWrapper.ClaimUnusedPRLs(completedUploads)
 	if err != nil {
 		t.Fatal("Failed to claim PRLs")
 	} else {
@@ -368,4 +332,3 @@ func (s *EthereumTestSuite) claimUnusedPRL(t *testing.T) {
 	}
 
 }
-*/
