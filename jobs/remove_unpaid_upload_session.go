7721c2caa1be9e35b3000379602fdf9a56f81fab
Philip Zhao <pzhao5@uwalumni.com>
2018-05-26 19:44:47 -0400

    Update

--- jobs/remove_unpaid_upload_session.go
+++ jobs/remove_unpaid_upload_session.go
@@ -1,23 +1,21 @@
 package jobs
 
 import (
-	"github.com/oysterprotocol/brokernode/models"
-	"github.com/oysterprotocol/brokernode/utils"
 	"github.com/gobuffalo/pop"
+	"github.com/oysterprotocol/brokernode/models"
 	"github.com/oysterprotocol/brokernode/services"
+	"github.com/oysterprotocol/brokernode/utils"
 )
 
-// Allows client to override this value. For testing.
-var unpaidExpirationInHour = 24
-
-func init() {
-	SetUnpaidExpiration(0, true)
-}
+/*UnpaidExpirationInHour means number of hours before it should remove unpaid upload session. */
+const UnpaidExpirationInHour = 24
 
 /*RemoveUnpaidUploadSession cleans up unpload_sessions and data_maps talbe for expired/unpaid session. */
 func RemoveUnpaidUploadSession() {
 	sessions := []models.UploadSession{}
-	err := models.DB.RawQuery("SELECT * from upload_sessions WHERE payment_status != ?", models.PaymentStatusConfirmed).All(&sessions)
+	err := models.DB.RawQuery(
+		"SELECT * from upload_sessions WHERE payment_status != ? AND TIMESTAMPDIFF(hour, updated_at, NOW()) >= ?",
+		models.PaymentStatusConfirmed, UnpaidExpirationInHour).All(&sessions)
 	if err != nil {
 		oyster_utils.LogIfError(err)
 		return
@@ -29,25 +27,15 @@ func RemoveUnpaidUploadSession() {
 			continue
 		}
 
-		oyster_utils.LogIfError(models.DB.Transaction(func(tx *pop.Connection) error {
-			if err := tx.RawQuery("DELETE from data_maps WHERE genesis_hash = ?", sessions.GenesisHash).All(&[]models.DataMaps{});
-				 err != nil {
+		err := models.DB.Transaction(func(tx *pop.Connection) error {
+			if err := tx.RawQuery("DELETE from data_maps WHERE genesis_hash = ?", session.GenesisHash).All(&[]models.DataMap{}); err != nil {
 				return nil
 			}
-			if err := tx.RawQuery("DELETE from upload_sessions WHERE id = ?", session.ID).All(&[]models.UploadSession{});
-				 err != nil {
+			if err := tx.RawQuery("DELETE from upload_sessions WHERE id = ?", session.ID).All(&[]models.UploadSession{}); err != nil {
 				return err
 			}
-			return nil			
+			return nil
 		})
-	}
-}
-
-/*SetUnpaidExpiration allows caller to control how long the expiration duration. */
-func SetUnpaidExpiration(hour int, setToDefault bool) {
-	if setToDefault {
-		unpaidExpirationInHour = 24
-	} else {
-		unpaidExpirationInHour = hour
+		oyster_utils.LogIfError(err)
 	}
 }
--- jobs/remove_unpaid_upload_session_test.go
+++ jobs/remove_unpaid_upload_session_test.go
@@ -1,17 +1,119 @@
 package jobs_test
 
 import (
-	"github.com/gobuffalo/pop/nulls"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/oysterprotocol/brokernode/jobs"
 	"github.com/oysterprotocol/brokernode/models"
+	"github.com/oysterprotocol/brokernode/services"
+	"github.com/oysterprotocol/brokernode/utils"
 )
 
-func (suite *JobsSuite) Test_RemoveUnpaid_completedSessions() {
+func (suite *JobsSuite) Test_RemoveUnpaid_uploadSessionsAndDataMap() {
+	jobs.EthWrapper = services.Eth{
+		CheckBalance: func(addr common.Address) *big.Int {
+			return big.NewInt(0)
+		},
+	}
+
+	addStartUploadSession(suite, "UploadSessionsAndDataMap_Expired", models.PaymentStatusInvoiced, true)
+	addStartUploadSession(suite, "UploadSessionsAndDataMap_NoExpired", models.PaymentStatusInvoiced, false)
+
+	jobs.RemoveUnpaidUploadSession()
+
+	verifyData(suite, "UploadSessionsAndDataMap_NoExpired")
 }
 
-func (suite *JobsSuite) Test_RemoveUnpaid_withoutDataMap() {
+func (suite *JobsSuite) Test_RemoveUnpaid_allPaid() {
+	addStartUploadSession(suite, "AllPaid", models.PaymentStatusConfirmed, true)
+
+	jobs.RemoveUnpaidUploadSession()
+
+	verifyData(suite, "AllPaid")
 }
 
 func (suite *JobsSuite) Test_RemoveUnpaid_hasBalance() {
+	jobs.EthWrapper = services.Eth{
+		CheckBalance: func(addr common.Address) *big.Int {
+			return big.NewInt(10)
+		},
+	}
 
+	addStartUploadSession(suite, "HasBalance", models.PaymentStatusInvoiced, true)
+
+	jobs.RemoveUnpaidUploadSession()
+
+	verifyData(suite, "HasBalance")
+}
+
+func (suite *JobsSuite) Test_RemoveUnpaid_OnlyRemoveUploadSession() {
+	jobs.EthWrapper = services.Eth{
+		CheckBalance: func(addr common.Address) *big.Int {
+			return big.NewInt(0)
+		},
+	}
+
+	addOnlySession(suite, "OnlyRemoveUploadSession_Expired", models.PaymentStatusInvoiced, true)
+	addOnlySession(suite, "OnlyRemoveUploadSession_NoExpired", models.PaymentStatusInvoiced, false)
+
+	jobs.RemoveUnpaidUploadSession()
+
+	verifyData(suite, "OnlyRemoveUploadSession_NoExpired")
+}
+
+func addStartUploadSession(suite *JobsSuite, genesisHash string, paymentStatus int, isExpired bool) {
+	session := models.UploadSession{
+		GenesisHash:    genesisHash,
+		FileSizeBytes:  8000,
+		NumChunks:      2,
+		Type:           models.SessionTypeAlpha,
+		PaymentStatus:  paymentStatus,
+		TreasureStatus: models.TreasureInDataMapComplete,
+	}
+	_, err := session.StartUploadSession()
+	suite.Nil(err)
+
+	if isExpired {
+		exceedLimitUpdateTime := time.Now().Add(-(jobs.UnpaidExpirationInHour + 1) * time.Hour)
+		// Force to update updated_at field
+		err = suite.DB.RawQuery("UPDATE upload_sessions SET updated_at = ? WHERE id = ?",
+			exceedLimitUpdateTime.Format(oyster_utils.SqlTimeFormat), session.ID).All(&[]models.UploadSession{})
+		suite.Nil(err)
+	}
+}
+
+func addOnlySession(suite *JobsSuite, genesisHash string, paymentStatus int, isExpired bool) {
+	session := models.UploadSession{
+		GenesisHash:    genesisHash,
+		FileSizeBytes:  8000,
+		NumChunks:      2,
+		Type:           models.SessionTypeAlpha,
+		PaymentStatus:  paymentStatus,
+		TreasureStatus: models.TreasureInDataMapComplete,
+	}
+	_, err := suite.DB.ValidateAndCreate(&session)
+	suite.Nil(err)
+
+	if isExpired {
+		exceedLimitUpdateTime := time.Now().Add(-(jobs.UnpaidExpirationInHour + 1) * time.Hour)
+		// Force to update updated_at field
+		err = suite.DB.RawQuery("UPDATE upload_sessions SET updated_at = ? WHERE id = ?",
+			exceedLimitUpdateTime.Format(oyster_utils.SqlTimeFormat), session.ID).All(&[]models.UploadSession{})
+		suite.Nil(err)
+	}
+}
+
+func verifyData(suite *JobsSuite, expectedDenesisHash string) {
+	var sessions []models.UploadSession
+	suite.Nil(suite.DB.RawQuery("SELECT * from upload_sessions").All(&sessions))
+	suite.Equal(1, len(sessions))
+	suite.Equal(expectedDenesisHash, sessions[0].GenesisHash)
+
+	var dataMaps []models.DataMap
+	suite.Nil(suite.DB.RawQuery("SELECT * from data_maps").All(&dataMaps))
+	for _, dataMap := range dataMaps {
+		suite.Equal(expectedDenesisHash, dataMap.GenesisHash)
+	}
 }
