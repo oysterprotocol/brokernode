1f6db7fcfe8e1db921b7092d112d0ac99339fcaf
Philip Zhao <philipzhao@Philips-MBP.jetpack>
2018-06-10 11:37:21 -0400

    Clean util dir

--- utils/test_utils.go
+++ utils/test_utils.go
@@ -5,25 +5,36 @@ import (
 	"testing"
 )
 
-func assertStringEqual(v string, expect string, t *testing.T) {
+/*AssertStringEqual fails if v != expect.*/
+func AssertStringEqual(v string, expect string, t *testing.T) {
 	if v != expect {
 		t.Errorf("expect '%s' does not equal to %s", expect, v)
 	}
 }
 
-func assertError(e error, t *testing.T) {
-	if e == nil {
-		t.Error("Expect error but not error")
+/*AssertNoError expects not error and fails if e != nil.*/
+func AssertNoError(e error, t *testing.T, desc string) {
+	if e != nil {
+		t.Errorf("Error unexpected: %v, error: %v", desc, e)
 	}
 }
 
-func assertTrue(v bool, t *testing.T, desc string) {
+/*AssertError expects e as NOT nil, fails on e == nil.*/
+func AssertError(e error, t *testing.T, desc string) {
+	if e == nil {
+		t.Errorf("Expected an error: %v", desc)
+	}
+}
+
+/*AssertTrue fails if v is false.*/
+func AssertTrue(v bool, t *testing.T, desc string) {
 	if !v {
 		t.Error(desc)
 	}
 }
 
-func assertContainString(v string, substr string, t *testing.T) {
+/*AssertContainString fails if substr is not part of v.*/
+func AssertContainString(v string, substr string, t *testing.T) {
 	if !strings.Contains(v, substr) {
 		t.Errorf("%s does not contain substring %s", v, substr)
 	}
