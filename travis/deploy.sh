a109803fd9e94f55f9c679513147c920e5b2d3be
Edmund Mai <edmundmai@gmail.com>
2018-06-25 18:11:05 -0400

    again

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ssh ubuntu@52.14.218.135 -i ./id_rsa <<EOF
+echo "HEREEEEEEEEEEEEEEEE"
+pwd
+
+ssh ubuntu@52.14.218.135 -i ./travis/id_rsa <<EOF
   cd brokernode
   echo "PRINT SOMETHINGGGGGGGGGGGGGGGGG"
 EOF
