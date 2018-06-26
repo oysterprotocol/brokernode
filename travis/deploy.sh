2b3df20faa5ce420472e753e6e9d9953d618e6a3
Edmund Mai <edmundmai@gmail.com>
2018-06-26 16:48:34 -0400

    more

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,6 +3,6 @@
 echo "HEREEEEEEEEEEEEEEEE"
 pwd
 
-ssh ubuntu@52.14.218.135 -i ./travis/id_rsa <<EOF
+ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa <<EOF
   echo "PRINT SOMETHINGGGGGGGGGGGGGGGGG"
 EOF
