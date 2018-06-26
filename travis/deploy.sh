47202a63da6a6b79108aaee93ae75c0ea58639b8
Edmund Mai <edmundmai@gmail.com>
2018-06-26 17:00:13 -0400

    less open priv key file

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,6 +3,8 @@
 echo "HEREEEEEEEEEEEEEEEE"
 pwd
 
+chmod 600 ./travis/id_rsa
+
 ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa <<EOF
   echo "PRINT SOMETHINGGGGGGGGGGGGGGGGG"
 EOF
