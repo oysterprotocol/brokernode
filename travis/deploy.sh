b808e3cc60d3ee307db01b841965f5e41df06fbd
Edmund Mai <edmundmai@gmail.com>
2018-06-27 16:52:54 -0400

    testing

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -10,14 +10,6 @@ script=$(cat <<-END
   cp /home/ubuntu/database.dev.yml ./database.yml;
   cp /home/ubuntu/docker-compose.dev.yml ./docker-compose.yml;
   echo "ppppppppppppppp";
-  docker stop $(docker ps -aq);
-  echo "yyyyyyyyyyyyy";
-  docker rm $(docker ps -aq);
-  echo "mmmmmmmmmmmm";
-  docker rmi $(docker images -q);
-  echo "xxxxxxxxxxxxxxxx";
-  DEBUG=1 docker-compose up --build -d;
-  echo "Done!";
 END
 )
 
