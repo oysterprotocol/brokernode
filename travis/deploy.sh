5404f475e782540aca93191be4de964764eb0866
Edmund Mai <edmundmai@gmail.com>
2018-06-27 11:29:42 -0400

    refactor script into a var

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,6 +5,10 @@ pwd
 
 chmod 600 ./travis/id_rsa
 
-ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa <<EOF
+script=$(cat <<-END
   echo "PRINT SOMETHINGGGGGGGGGGGGGGGGG"
-EOF
+  cd brokernode
+END
+)
+
+ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa $script
