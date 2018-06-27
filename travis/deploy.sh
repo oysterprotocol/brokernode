a3d4b6772cb2f5263ad7f67c9ade2230370c06fe
Edmund Mai <edmundmai@gmail.com>
2018-06-27 14:49:51 -0400

    getting close

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,7 +6,14 @@ pwd
 chmod 600 ./travis/id_rsa
 
 script=$(cat <<-END
-  cd brokernode;
+  sudo su;
+  cd /home/ubuntu/brokernode;
+  git stash;
+  git pull;
+  cp /home/ubuntu/database.dev.yml ./database.yml;
+  cp /home/ubuntu/docker-compose.dev.yml ./docker-compose.yml;
+  docker stop $(docker ps -aq);
+  docker rm $(docker ps -aq);
   docker rmi $(docker images -q);
   DEBUG=1 docker-compose up --build -d;
 END
