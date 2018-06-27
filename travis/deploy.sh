1a977b1020203f3092aa8f14a7a520891769556a
Edmund Mai <edmundmai@gmail.com>
2018-06-27 15:59:08 -0400

    try again

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "HEREEEEEEEEEEEEEEEE"
-pwd
-
 chmod 600 ./travis/id_rsa
 
 script=$(cat <<-END
@@ -12,10 +9,15 @@ script=$(cat <<-END
   git pull;
   cp /home/ubuntu/database.dev.yml ./database.yml;
   cp /home/ubuntu/docker-compose.dev.yml ./docker-compose.yml;
+  echo "ppppppppppppppp";
   docker stop $(docker ps -aq);
+  echo "yyyyyyyyyyyyy";
   docker rm $(docker ps -aq);
+  echo "mmmmmmmmmmmm";
   docker rmi $(docker images -q);
+  echo "xxxxxxxxxxxxxxxx";
   DEBUG=1 docker-compose up --build -d;
+  echo "Done!";
 END
 )
 
