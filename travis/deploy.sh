641a528b3f3fc12913d578c63a9e48ebff0fb413
Edmund Mai <edmundmai@gmail.com>
2018-06-29 11:23:44 -0400

    try using var

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,21 +6,6 @@ script=$(cat <<-END
   sudo su;
   cd /home/ubuntu/brokernode;
   git stash;
-  git pull;
-  cp /home/ubuntu/database.dev.yml ./database.yml;
-  cp /home/ubuntu/docker-compose.dev.yml ./docker-compose.yml;
-  docker stop $(docker ps -aq);
-  docker rm $(docker ps -aq);
-  docker rmi $(docker images -q);
-  DEBUG=1 docker-compose up --build -d;
-  echo "Done!";
-END
-)
-
-ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa <<-END
-  sudo su;
-  cd /home/ubuntu/brokernode;
-  git stash;
   git clean -f -d;
   git checkout master;
   git pull;
@@ -32,3 +17,6 @@ ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa <<-END
   DEBUG=1 docker-compose up --build -d;
   echo "Done!";
 END
+)
+
+ssh -o StrictHostKeyChecking=no ubuntu@52.14.218.135 -i ./travis/id_rsa $script
