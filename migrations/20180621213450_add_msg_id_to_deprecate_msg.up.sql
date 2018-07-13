23ebc1b6e42997db09cb6af14ce9615cff60d09b
Rebel Fornea <rebelfornea@gmail.com>
2018-07-13 14:15:04 -0400

    Put these in separate files

--- migrations/20180621213450_add_msg_id_to_deprecate_msg.up.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `treasures`
-ADD msg_id VARCHAR (255) DEFAULT NULL;
-
-ALTER TABLE `completed_data_maps`
-ADD msg_id VARCHAR (255) DEFAULT NULL;
-
-ALTER TABLE `data_maps`
-ADD msg_id VARCHAR (255) NOT NULL;
