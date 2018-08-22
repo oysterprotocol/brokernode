ca166ec9df01b3a7b39b52756ec77dc0f9c6e0a3
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-08-22 10:13:04 -0400

    Allow usage of multiple cores, not just green threads

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"math/rand"
+	"runtime"
 	"time"
 
 	"github.com/gobuffalo/pop"
@@ -11,6 +12,10 @@ import (
 )
 
 func main() {
+	// Allow running on multiple cores. Kinda weird that this is manual?
+	// https://golang.org/pkg/runtime/#GOMAXPROCS
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	pop.Debug = false
 	// Setup rand. See https://til.hashrocket.com/posts/355f31f19c-seeding-golangs-rand
 	rand.Seed(time.Now().Unix())
