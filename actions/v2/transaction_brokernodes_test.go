26cb5b32b958b796a14b9c5708faae86dbe43ae4
Edmund Mai <edmundmai@gmail.com>
2018-10-03 10:58:03 -0400

    Revert "add copies of controllers v2 folder"
    This reverts commit acd01fe50204adfa71f7625a5f469f25ef1432a4.

--- actions/v2/transaction_brokernodes_test.go
+++
@@ -1,4 +0,0 @@
-package actions
-
-func (suite *ActionSuite) Test_Create_Transaction_brokernodes() {}
-func (suite *ActionSuite) Test_Update_Transaction_brokernodes() {}
--- actions/v2/transaction_genesis_hashes_test.go
+++
@@ -1,4 +0,0 @@
-package actions
-
-func (suite *ActionSuite) Test_Create_Transaction_genesis_hashes() {}
-func (suite *ActionSuite) Test_Update_Transaction_genesis_hashes() {}
