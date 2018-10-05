a73446dd8d0233f92a0e08e269b2398cbb61dc06
Peisen Zhao <philipzhao@Peisens-MBP.fios-router.home>
2018-10-04 21:49:38 -0400

    Setup basic V3 Controller for S3.

---
+++ actions/v3/actions_test.go
@@ -0,0 +1,20 @@
+package actions_v3
+
+import (
+	"github.com/gobuffalo/suite"
+	"github.com/oysterprotocol/brokernode/services"
+	"github.com/oysterprotocol/brokernode/utils"
+)
+
+type ActionSuite struct {
+	*suite.Action
+}
+
+func (suite *ActionSuite) SetupTest() {
+	suite.Action.SetupTest()
+
+	suite.Nil(oyster_utils.InitKvStore())
+
+	EthWrapper = services.EthWrapper
+	IotaWrapper = services.IotaWrapper
+}
