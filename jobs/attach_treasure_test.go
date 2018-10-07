4f0519e6f35ab09689fe6bd4106ca4e9d436fe21
Rebel Fornea <rebelfornea@gmail.com>
2018-10-07 15:33:07 -0400

    skeleton of test methods

--- jobs/attach_treasure_test.go
+++ jobs/attach_treasure_test.go
@@ -1,1 +1,29 @@
-package models
+package jobs_test
+
+func (suite *JobsSuite) Test_AttachTreasureTransactions_success() {
+
+}
+
+func (suite *JobsSuite) Test_AttachTreasureTransactions_error() {
+
+}
+
+func (suite *JobsSuite) Test_VerifyTreasureTransactions_error_while_verifying() {
+
+}
+
+func (suite *JobsSuite) Test_VerifyTreasureTransactions_success() {
+
+}
+
+func (suite *JobsSuite) Test_VerifyTreasureTransactions_does_not_match_record() {
+
+}
+
+func (suite *JobsSuite) Test_VerifyTreasureTransactions_not_attached_not_timed_out() {
+
+}
+
+func (suite *JobsSuite) Test_VerifyTreasureTransactions_not_attached_timed_out() {
+
+}
