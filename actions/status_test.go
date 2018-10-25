3ea16ed92a5dd75d9cd77fdc4c5b0de2cc73b6e3
Rebel Fornea <rebelfornea@gmail.com>
2018-10-25 13:51:40 -0400

    Make status accessible on `/status` instead of just `/api/v2/status`.  A future PR will move status to the main actions folder and deprecate `/api/v2/status` endpoint, after we have updated where the client points.

--- actions/status_test.go
+++ actions/status_test.go
@@ -6,6 +6,22 @@ import (
 )
 
 func (suite *ActionSuite) Test_CheckStatus() {
+	res := suite.JSON("/status").Get()
+
+	suite.Equal(200, res.Code)
+
+	// Parse response
+	resParsed := checkStatusRes{}
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	suite.Nil(err)
+	err = json.Unmarshal(bodyBytes, &resParsed)
+	suite.Nil(err)
+
+	suite.Equal(true, resParsed.Available == true || resParsed.Available == false)
+	suite.Equal(true, resParsed.NumChunksLimit == -1 || resParsed.NumChunksLimit > 0)
+}
+
+func (suite *ActionSuite) Test_CheckStatus_on_v2_endpoint() {
 	res := suite.JSON("/api/v2/status").Get()
 
 	suite.Equal(200, res.Code)
